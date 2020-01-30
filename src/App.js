13429940e3dd2893b5e71786abe6407d6b6fa2d7
HL <hoanglinh171038@gmail.com>
2020-01-30 21:08:36 +0700

    empty-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
 import React from "react";
 
-import "./App.css";
-import Products from "./components/Products";
-import Details from "./components/Details";
-import Carts from "./components/Carts";
-import Navbar from "./components/Navbar";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-import { Button } from "@material-ui/core";
-
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Navbar></Navbar>
-
-        <Route exact path="/" component={Products} />
-        <Route path="/products" component={Products} />
-        <Route path="/details" component={Details} />
-        <Route path="/carts" component={Carts} />
-      </div>
-    </BrowserRouter>
+    <div>hello linh</div>
   );
 }
 
